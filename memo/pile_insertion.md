ade8ee69b6735a5b34bc30eb953a1929c6658c99
MS-Ubuntu <shigamasataka@gmail.com>
2023-06-28 09:45:01 +0900

    2023/06/28 9:44 - 杭挿入のデモコード作成中

--- memo/pile_insertion.md
+++ memo/pile_insertion.md
@@ -1,3 +1,15 @@
+# 2023/06/28
+- 杭挿入のコードを書く
+- 初期変数をまとめて出力するためのメソッドがあるはず。
+    - [`Omega.save`](https://yade-dem.org/doc/yade.wrapper.html#yade.wrapper.Omega.save)が該当
+- ただ外部で定義した変数を上記のメソッドで保存する方法はないみたい
+- [ここ](https://gitlab.com/yade-dev/trunk/blob/master/examples/rod-penetration/model.py)にロッドを挿入したサンプルコードが書かれている
+- また[ここ](https://gitlab.com/yade-dev/trunk/blob/master/examples/PeriodicBoundaries/periodicSandPile.py)に横方向に周期境界を持つ場合に重力堆積させたコードがある
+    - このコードでは全体を周期境界としつつも、底面にだけごくごく薄いBoxを挿入することで底面がある箱を表現している
+
+
+
+
 # 2023/06/22
 
 ## そもそも杭挿入ができるのか
@@ -7,4 +19,3 @@
 - `cylinder`について
     - https://www.youtube.com/watch?v=Hh6nGzIU1vU&ab_channel=JanekKozicki
     - こういうもの。形状はものすごく近い
-- つまりこの形状を内部挿入して、
--- sample_code/pile_insertion.py
+++ sample_code/pile_insertion.py
@@ -1,93 +1,55 @@
-from yade import pack
+from __future__ import print_function
+from yade import pack, plot
 from yade.gridpfacet import *
+import numpy as np
+import datetime
+from pathlib import Path
+import os
+import sys
 
-dp = 6e-3
+# Dividing Lines to make the output more comprehensive
+print("")
+print("-------------------------------------")
 
-periodic = 1
-cylinderBottom = 0
-cylindersBottom = 1
+############## Constant and Variable ##############
 
-ground = 0.2*dp
-H = 60*dp
-L = 20*dp
+############## Contact Model ##############
 
+############## Boundary Box ##############
+# TODO: 周期境界にしたい
+O.periodic = True
+O.cell.hSize = Matrix3(1, 0, 0,
+                       0, 1, 0,
+                       0, 0, 1)
+
+lowBox = box(center=(0.5, 0.005, 0.5), extents=(5, 0.005, 5),
+             fixed=True, wire=False)
+O.bodies.append(lowBox)
+
+
+############## Sphere ##############
+sp = pack.SpherePack()
+sp.makeCloud((0.05, 0.5, 0.05), (0.95, 0.95, 0.95), rMean=.02, rRelFuzz=0)
+sp.toSimulation()
+
+############## Engine ##############
+# "allowBiggerThanPeriod=True" is needed to avoid particle falling down out of the box
 O.engines = [
     ForceResetter(),
-    InsertionSortCollider([Bo1_Sphere_Aabb(), Bo1_Box_Aabb(
-    ), Bo1_GridConnection_Aabb()], allowBiggerThanPeriod=True),
+    InsertionSortCollider(
+        [Bo1_Sphere_Aabb(), Bo1_Box_Aabb()], allowBiggerThanPeriod=True),
     InteractionLoop(
-        [Ig2_Sphere_Sphere_ScGeom(), Ig2_Box_Sphere_ScGeom(),
-         Ig2_Sphere_GridConnection_ScGridCoGeom()],
-        [Ip2_ViscElMat_ViscElMat_ViscElPhys()],
-        [Law2_ScGeom_ViscElPhys_Basic()]
-        # [Ip2_FrictMat_FrictMat_FrictPhys()],
-        # [Law2_ScGeom_FrictPhys_CundallStrack()]
+        [Ig2_Sphere_Sphere_ScGeom(), Ig2_Box_Sphere_ScGeom()],
+        [Ip2_FrictMat_FrictMat_FrictPhys()],
+        [Law2_ScGeom_FrictPhys_CundallStrack()]
     ),
-    PyRunner(command='check()', virtPeriod=0.1),
-    NewtonIntegrator(damping=0., gravity=(0, 0, -9.81))
+    NewtonIntegrator(gravity=(0, -9.81, 0), damping=0.4),
+    PyRunner(iterPeriod=10, command="checkUnbalncedForce()"),
 ]
 
-# Material creation
-O.materials.append(ViscElMat(en=0.5, et=1., young=5e6,
-                   poisson=0.5, density=2500., frictionAngle=0.4, label='Mat'))
-# O.materials.append(FrictMat(young = 5e6, poisson = 0.5, density=2500,frictionAngle=0.4, label='Mat'))
-
-# Periodic Cell or containing box
-if periodic:
-    O.periodic = True
-    O.cell.setBox(L, L, H)
-    O.bodies.append(box(center=(L/2., L/2., ground), extents=(100, 100, 0),
-                    fixed=True, color=(0., 1., 0.), wire=True, material='Mat'))
-else:
-    O.bodies.append(box(center=(L/2., L/2., ground), extents=(100, 100, 0), wire=True,
-                    fixed=True, color=(0., 1., 0.), material='Mat'))  # Made invisible to see below
-    O.bodies.append(box(center=(0, L/2., H/2.), extents=(0, L/2., H/2.), wire=True,
-                    fixed=True, color=(0., 1., 0.), material='Mat'))  # Made invisible to see
-    O.bodies.append(box(center=(L, L/2., H/2.), extents=(0, L/2.,
-                    H/2.), fixed=True, color=(0., 1., 0.), material='Mat'))
-    O.bodies.append(box(center=(L/2., 0, H/2.), extents=(L/2., 0, H/2.), wire=True,
-                    fixed=True, color=(0., 1., 0.), material='Mat'))  # Made invisible to see
-    O.bodies.append(box(center=(L/2., L, H/2.), extents=(L/2.,
-                    0, H/2.), fixed=True, color=(0., 1., 0.), material='Mat'))
-
-# Bottom fixed cylinder or fixed particles
-if cylinderBottom:
-    n = len(O.bodies)
-    cylinder(begin=(L/2., 100*L+0.001*dp, ground+dp/2.), end=(L/2., -100*L+0.001*dp, ground+dp/2.), radius=dp /
-             2., fixed=True, color=(0, 0, 1), intMaterial='Mat', extMaterial='Mat')  # Made invisible to see inside
-else:
-    if cylindersBottom:
-        ep = 0.001*dp  # a small thing
-        nodes = [[L/2., ep, ground+dp/2.], [L/2., 0.33*L+ep, ground+dp/2.],
-                 [L/2., 0.33*2*L+ep, ground+dp/2.], [L/2., L+ep, ground+dp/2.]]
-        cylinderConnection([[L/2., ep, ground+dp/2.], [L/2., 0.33*L+ep, ground+dp/2.], [
-                           L/2., 0.33*2*L+ep, ground+dp/2.], [L/2., L+ep, ground+dp/2.]], dp/2., fixed=True)
-    else:
-        for n in range(0, int(L/dp)+1):
-            O.bodies.append(sphere(center=(L/2., n*dp, ground+dp/2.), radius=dp/2., fixed=True,
-                            color=(0, 0, 1), wire=True, material='Mat'))  # Made invisible to see inside
-
-# Particle cloud for gravity deposition
-partCloud = pack.SpherePack()
-partCloud.makeCloud(minCorner=(4*L/10., 4*L/10., ground+3.*dp),
-                    maxCorner=(6*L/10., 6*L/10., 0.9*H), rRelFuzz=0., rMean=dp/2., num=200)
-partCloud.toSimulation(material='Mat')
-
-O.dt = 1e-5
-O.saveTmp()
-
-# Function to check if the center of a particle is contained inside the cylinder or pseudo cylinder
+O.dt = .5 * PWaveTimeStep()
+O.trackEnergy = True
 
 
-def check():
-    for b in O.bodies:
-        if b.dynamic and b.state.pos[2] < ground+dp:
-            posCylXZ = Vector3(L/2., 0., ground+dp/2.)
-            pRelX = b.state.pos[0] % L - posCylXZ[0]
-            pRelZ = b.state.pos[2] - posCylXZ[2]
-            # If the particle center is closer than the radius (i.e. if it is completely inside!)
-            if sqrt(pow(pRelX, 2) + pow(pRelZ, 2)) < dp/2.:
-                delta = dp/2.-sqrt(pow(pRelX, 2) + pow(pRelZ, 2))
-                print('particle {0} is inside the cylinder from an amount of {1} diameter'.format(
-                    b.id, delta/dp))
-                O.pause()
+def checkUnbalncedForce():
+    print(utils.unbalancedForce())
