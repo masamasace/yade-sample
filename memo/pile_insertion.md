b5179e2b3a7a2d11b57650b5f0b59bfea4ba0b55
MS-Ubuntu <shigamasataka@gmail.com>
2023-06-28 12:32:21 +0900

    2023/06/28 12:30 - 一応かんたんな杭挿入をシミュレーションできるようなコードが完成 - 複数の杭や針貫入試験をどのようにシミュレーションしていくのかをもう少し考えないと行けない - 過去の論文を参考にしてみる

--- memo/pile_insertion.md
+++ memo/pile_insertion.md
@@ -1,4 +1,8 @@
 # 2023/06/28
+
+## 次に向けてのTODO
+- 何回かに分けて粒子を堆積させて、初期高さと平均密度をコントロールする
+## メモ
 - 杭挿入のコードを書く
 - 初期変数をまとめて出力するためのメソッドがあるはず。
     - [`Omega.save`](https://yade-dem.org/doc/yade.wrapper.html#yade.wrapper.Omega.save)が該当
@@ -6,6 +10,12 @@
 - [ここ](https://gitlab.com/yade-dev/trunk/blob/master/examples/rod-penetration/model.py)にロッドを挿入したサンプルコードが書かれている
 - また[ここ](https://gitlab.com/yade-dev/trunk/blob/master/examples/PeriodicBoundaries/periodicSandPile.py)に横方向に周期境界を持つ場合に重力堆積させたコードがある
     - このコードでは全体を周期境界としつつも、底面にだけごくごく薄いBoxを挿入することで底面がある箱を表現している
+    - `allowBiggerThanPeriod=True`がないと、周期境界の要素よりも小さい底面の箱しかおけず、粒子がこぼれ落ちてしまう。
+- どうやら`cylinder`を使うと`yade.gridpfacet`が呼び出されてしまうらしい
+    - 詳しくは[こちら](https://yade-dem.org/doc/yade.gridpfacet.html?highlight=cylid)
+- `cylinder`でどのように力が計算されているのかがわからない...
+    - 要検討
+
 
 
 
--- sample_code/pile_insertion.py
+++ sample_code/pile_insertion.py
@@ -1,3 +1,5 @@
+
+
 from __future__ import print_function
 from yade import pack, plot
 from yade.gridpfacet import *
@@ -12,28 +14,35 @@ print("")
 print("-------------------------------------")
 
 ############## Constant and Variable ##############
+pile_height = 0.5
+state_index = 0
 
 ############## Contact Model ##############
 
 ############## Boundary Box ##############
-# TODO: 周期境界にしたい
 O.periodic = True
 O.cell.hSize = Matrix3(1, 0, 0,
-                       0, 1, 0,
+                       0, 3, 0,
                        0, 0, 1)
 
 lowBox = box(center=(0.5, 0.005, 0.5), extents=(5, 0.005, 5),
              fixed=True, wire=False)
 O.bodies.append(lowBox)
 
-
 ############## Sphere ##############
 sp = pack.SpherePack()
-sp.makeCloud((0.05, 0.5, 0.05), (0.95, 0.95, 0.95), rMean=.02, rRelFuzz=0)
+sp.makeCloud((0, 0, 0), (1, 1, 1), rMean=.04, rRelFuzz=0, seed=1)
 sp.toSimulation()
 
+############## Sphere ##############
+cylIds = []
+nodesIds = []
+pile = cylinder((0.5, 1.25, 0.5),(0.5, 1.25+pile_height, 0.5),
+                cylIds=cylIds, nodesIds=nodesIds, radius=0.2, fixed=True)
+print(cylIds)
+
 ############## Engine ##############
-# "allowBiggerThanPeriod=True" is needed to avoid particle falling down out of the box
+# "allowBiggerThanPeriod=True" in InsertionSortCollider is needed to avoid spheres from falling down out of the box
 O.engines = [
     ForceResetter(),
     InsertionSortCollider(
@@ -44,12 +53,48 @@ O.engines = [
         [Law2_ScGeom_FrictPhys_CundallStrack()]
     ),
     NewtonIntegrator(gravity=(0, -9.81, 0), damping=0.4),
-    PyRunner(iterPeriod=10, command="checkUnbalncedForce()"),
+    PyRunner(iterPeriod=100, command="ShowValue()"),
+    PyRunner(iterPeriod=1, command="ChangeState()")
 ]
 
 O.dt = .5 * PWaveTimeStep()
 O.trackEnergy = True
 
+# print(O.bodies[cylIds[0]].shape.node1.shape.dict())
+print(nodesIds)
 
-def checkUnbalncedForce():
-    print(utils.unbalancedForce())
+def ShowValue():
+    # energy_dict = dict(O.energy.items())
+    
+    output_str = "UnF: " + '{:> 5.2f}'.format(utils.unbalancedForce()) + \
+                 " Pos: " + '{:> 6.3f}'.format(O.bodies[cylIds[0]].state.pos[1]) + \
+                 " Fx_cy: " + '{:> 8.2f}'.format(O.forces.f(cylIds[0])[0]) + \
+                 " Fy_cy: " + '{:> 8.2f}'.format(O.forces.f(cylIds[0])[1]) + \
+                 " Fz_cy: " + '{:> 8.2f}'.format(O.forces.f(cylIds[0])[2]) + \
+                 " Fx_no: " + '{:> 8.2f}'.format(O.forces.f(nodesIds[0])[0]) + \
+                 " Fy_no: " + '{:> 8.2f}'.format(O.forces.f(nodesIds[0])[1]) + \
+                 " Fz_no: " + '{:> 8.2f}'.format(O.forces.f(nodesIds[0])[2])  
+    
+    print(output_str)
+    # print(utils.unbalancedForce(), O.bodies[cylIds[0]].state.pos,O.bodies[cylIds[0]].dynamic)
+
+
+def ChangeState():
+    global state_index
+    # print(O.forces.f(cylIds[0]), O.forces.t(cylIds[0]))
+    # print(utils.bodyStressTensors()[cylIds[0]])
+    # print(O.bodies[cylIds[0]].state.dict())
+    # print(utils.unbalancedForce())
+    if state_index == 0:
+        if utils.unbalancedForce() < 0.1 and O.iter > 10000:
+            O.bodies[cylIds[0]].state.blockedDOFs = "xyzXYZ"
+            O.bodies[cylIds[0]].state.vel = Vector3(0, -0.2, 0)
+            O.bodies[nodesIds[0]].state.blockedDOFs = "xyzXYZ"
+            O.bodies[nodesIds[0]].state.vel = Vector3(0, -0.2, 0)
+            O.bodies[nodesIds[1]].state.blockedDOFs = "xyzXYZ"
+            O.bodies[nodesIds[1]].state.vel = Vector3(0, -0.2, 0)
+            state_index = 1
+    elif  state_index == 1:
+        if O.bodies[cylIds[0]].state.pos[1] <= pile_height / 2:
+            O.pause()
+        
