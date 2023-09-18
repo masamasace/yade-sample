6d48cfd12732823147c5743c63ca8e5357def47a
masamasace <shigamasataka@gmail.com>
2023-09-18 15:10:50 +0900

    modified:   post_process_code/a.out modified:   post_process_code/voxel_porosity.cu modified:   post_process_code/voxel_porosity_v2.cu modified:   sample_code/pile_insertion.py modified:   temp/sphere_pack_pile_insertion.txt

--- post_process_code/voxel_porosity.cu
+++ post_process_code/voxel_porosity.cu
@@ -1,8 +1,96 @@
-#include <string>
-#include <fstream>
+#include <iostream>
+#include <vector>
+#include <cuda_runtime.h>
 
-int main(){
-    std::string input_csv_file_path = "/media/eel-22/Data/01_Shiga/02_Yade/Yade-Cyclic-Constant-Volume/result/2023-09-14_20-48-17_success/CSV/dataset_000000.csv";
+// 定数としてボクセルのサイズを設定
+const float VOXEL_SIZE = 1.0;
 
-    std::ifstream ifs_csv_file(input_csv_file_path);
+// CUDAカーネル関数
+// 各ボクセルが粒状体によって占められているかどうかをチェックする
+__global__ void checkVoxels(float *particles, int num_particles, float *voxel_centers, int num_voxels, int *occupied) {
+    // 現在のスレッドのインデックスを計算
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    // インデックスがボクセルの総数以上の場合、このスレッドは何もしない
+    if (idx >= num_voxels) return;
+
+    // 現在のボクセルの中心座標を取得
+    float voxel_x = voxel_centers[3 * idx];
+    float voxel_y = voxel_centers[3 * idx + 1];
+    float voxel_z = voxel_centers[3 * idx + 2];
+
+    // 全ての粒状体に対して、ボクセルが占められているかどうかをチェック
+    for (int i = 0; i < num_particles; i++) {
+        // 粒状体の中心座標と半径を取得
+        float px = particles[5 * i + 1];
+        float py = particles[5 * i + 2];
+        float pz = particles[5 * i + 3];
+        float radius = particles[5 * i + 4];
+
+        // ボクセルの中心と粒状体の中心との距離の二乗を計算
+        float dist2 = (voxel_x - px) * (voxel_x - px) + (voxel_y - py) * (voxel_y - py) + (voxel_z - pz) * (voxel_z - pz);
+
+        // 距離が粒状体の半径以下ならボクセルは占められていると判断
+        if (dist2 <= radius * radius) {
+            occupied[idx] = 1;
+            return;
+        }
+    }
+
+    // どの粒状体にも占められていない場合、occupiedを0に設定
+    occupied[idx] = 0;
+}
+
+int main() {
+    std::vector<float> particles; // [id, x, y, z, radius]
+    // データの読み込み...
+
+    // 部分区画の座標を設定
+    float region_start_x = ...;
+    float region_end_x = ...;
+    // y, zも同様に
+
+    // 部分区画内のボクセルの総数を計算
+    int num_voxels = ...;
+    // 部分区画内の各ボクセルの中心座標を保存するための配列を準備
+    std::vector<float> voxel_centers(3 * num_voxels);
+    // 中心座標の計算...
+
+    // GPUメモリ上にボクセルが占められているかどうかを保存するための配列を確保
+    int *d_occupied;
+    cudaMalloc(&d_occupied, num_voxels * sizeof(int));
+
+    // GPUメモリ上に粒状体とボクセルのデータをコピーするためのポインタを確保
+    float *d_particles, *d_voxel_centers;
+    cudaMalloc(&d_particles, particles.size() * sizeof(float));
+    cudaMalloc(&d_voxel_centers, voxel_centers.size() * sizeof(float));
+
+    // ホストメモリからGPUメモリへデータをコピー
+    cudaMemcpy(d_particles, particles.data(), particles.size() * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_voxel_centers, voxel_centers.data(), voxel_centers.size() * sizeof(float), cudaMemcpyHostToDevice);
+
+    // スレッドの数とブロックの数を設定
+    int threads_per_block = 256;
+    int blocks = (num_voxels + threads_per_block - 1) / threads_per_block;
+
+    // CUDAカーネル関数を呼び出し
+    checkVoxels<<<blocks, threads_per_block>>>(d_particles, particles.size() / 5, d_voxel_centers, num_voxels, d_occupied);
+
+    // GPUメモリからホストメモリへ結果をコピー
+    std::vector<int> occupied(num_voxels);
+    cudaMemcpy(occupied.data(), d_occupied, num_voxels * sizeof(int), cudaMemcpyDeviceToHost);
+
+    // 空隙率を計算
+    int sum = 0;
+    for (int o : occupied) {
+        sum += o;
+    }
+    float porosity = 1.0 - float(sum) / num_voxels;
+    std::cout << "Porosity: " << porosity << std::endl;
+
+    // GPUメモリを解放
+    cudaFree(d_particles);
+    cudaFree(d_voxel_centers);
+    cudaFree(d_occupied);
+
+    return 0;
 }
--- post_process_code/voxel_porosity_v2.cu
+++ post_process_code/voxel_porosity_v2.cu
@@ -5,6 +5,8 @@
 #include <string>
 #include <filesystem>
 #include <list>
+#include <algorithm>
+#include <cmath>
 #include <cuda_runtime.h>
 
 
@@ -19,10 +21,10 @@ __global__ void checkVoxels(float *particles, int num_particles, float *voxel_ce
     float voxel_z = voxel_centers[3 * idx + 2];
 
     for (int i = 0; i < num_particles; i++) {
-        float px = particles[4 * i];
-        float py = particles[4 * i + 1];
-        float pz = particles[4 * i + 2];
-        float radius = particles[4 * i + 3];
+        float radius = particles[4 * i];
+        float px = particles[4 * i + 1];
+        float py = particles[4 * i + 2];
+        float pz = particles[4 * i + 3];
 
         float dist2 = (voxel_x - px) * (voxel_x - px) + (voxel_y - py) * (voxel_y - py) + (voxel_z - pz) * (voxel_z - pz);
         if (dist2 <= radius * radius) {
@@ -36,21 +38,40 @@ __global__ void checkVoxels(float *particles, int num_particles, float *voxel_ce
 std::vector<float> readCSV(const std::string &filename) {
     std::vector<float> particles;
     std::ifstream file(filename);
-    std::string line, value;
-    std::getline(file, line);  // headerを読み飛ばす
+    std::string line, value, header_label;
+    std::vector<std::string> target_header_labels;
+    std::vector<int> target_header_label_indexes;
+    
+    target_header_labels = {"radii", "Points:0", "Points:1", "Points:2"};
+    
+    std::getline(file, line);
+    std::istringstream header(line);
+    int cur_index = 0;
+
+    while (std::getline(header, header_label, ',')) {
+
+        for (std::string target_header_label : target_header_labels) {
+
+            if (header_label.find(target_header_label) != std::string::npos) {
+                target_header_label_indexes.push_back(cur_index);
+            }
+        }
+
+        cur_index++;
+    }
 
     while (std::getline(file, line)) {
         std::istringstream iss(line);
+
         std::vector<std::string> values;
         while (std::getline(iss, value, ',')) {
             values.push_back(value);
         }
-        particles.push_back(std::stof(values[0]));  // Points:0
-        particles.push_back(std::stof(values[1]));  // Points:1
-        particles.push_back(std::stof(values[2]));  // Points:2
-        particles.push_back(std::stof(values[3]));  // radii
 
-        std::cout << values[3] << std::endl;
+        particles.push_back(std::stof(values[target_header_label_indexes[0]]));
+        particles.push_back(std::stof(values[target_header_label_indexes[1]]));
+        particles.push_back(std::stof(values[target_header_label_indexes[2]]));
+        particles.push_back(std::stof(values[target_header_label_indexes[3]])); 
     }
     return particles;
 }
@@ -58,33 +79,89 @@ std::vector<float> readCSV(const std::string &filename) {
 int main() {
     const std::list<std::list<std::list<float>>> regions = {
         {{0.005, 0, 0.015}, {0.015, 0.01, 0.025}},
-        {{0.005, 0.01, 0.015}, {0.015, 0.02, 0.025}},
+        {{0.005, 0.09, 0.015}, {0.015, 0.1, 0.025}},
     };
 
-    const int voxel_resolution = 100;
+    const int voxel_resolution = 200;
+    const std::filesystem::path target_dir = "/media/eel-22/Data/01_Shiga/02_Yade/Yade-Cyclic-Constant-Volume/result/2023-09-14_20-48-17_success/CSV";
+
+    std::vector<std::filesystem::path> sorted_file_paths;
+
+    for (const auto &entry : fs::directory_iterator(target_dir)) {
+        sorted_file_paths.push_back(entry.path());
+    }
+
+    std::sort(sorted_file_paths.begin(), sorted_file_paths.end());
     
-    for (const auto &entry : fs::directory_iterator(".")) {
-        if (entry.path().extension() == ".csv") {
-            std::vector<float> particles = readCSV(entry.path());
+    for (const auto &path : sorted_file_paths) {
+        if (path.extension() == ".csv") {
+            std::vector<float> particles = readCSV(path);
+            int counter_region = 0;
 
             for (const auto &region : regions) {  // regionsは部分区画のリスト
 
-                auto [region_min_vec, region_max_vec] = region
+                std::vector<float> region_min_vec(region.front().begin(), region.front().end());
+                std::vector<float> region_max_vec(region.back().begin(), region.back().end());
+            
+                float offset_x = region_max_vec[0] - region_min_vec[0];
+                float offset_y = region_max_vec[1] - region_min_vec[1];
+                float offset_z = region_max_vec[2] - region_min_vec[2];
 
-                
-                // 部分区画の座標とボクセルの総数を計算...
+                std::vector<float> voxel_centers(std::pow(voxel_resolution, 3) * 3);
+
+                for (int i = 0; i < voxel_resolution; i++) {
+                    for (int j = 0; j < voxel_resolution; j++) {
+                        for (int k = 0; k < voxel_resolution; k++) {
+                            voxel_centers[(i * std::pow(voxel_resolution, 2) + j * voxel_resolution + k) * 3] = 
+                                region_min_vec[0] + float(i + 0.5) * offset_x / voxel_resolution;
+                            voxel_centers[(i * std::pow(voxel_resolution, 2) + j * voxel_resolution + k) * 3 + 1] = 
+                                region_min_vec[1] + float(j + 0.5) * offset_y / voxel_resolution;
+                            voxel_centers[(i * std::pow(voxel_resolution, 2) + j * voxel_resolution + k) * 3 + 2] = 
+                                region_min_vec[2] + float(k + 0.5) * offset_z / voxel_resolution;
+                        }
+                    }
+                }
 
                 // CUDA処理...
+                int *d_occupied;
+                cudaMalloc(&d_occupied, voxel_centers.size() * sizeof(int));
 
+                float *d_particles, *d_voxel_centers;
+                cudaMalloc(&d_particles, particles.size() * sizeof(float));
+                cudaMalloc(&d_voxel_centers, voxel_centers.size() * sizeof(float));
+
+                cudaMemcpy(d_particles, particles.data(), particles.size() * sizeof(float), cudaMemcpyHostToDevice);
+                cudaMemcpy(d_voxel_centers, voxel_centers.data(), voxel_centers.size() * sizeof(float), cudaMemcpyHostToDevice);
+
+                // スレッドの数とブロックの数を設定
+                int threads_per_block = 1024;
+                int blocks = (voxel_centers.size() / 3 + threads_per_block - 1) / threads_per_block;
+
+                // CUDAカーネル関数を呼び出し
+                checkVoxels<<<blocks, threads_per_block>>>(d_particles, particles.size() / 4, d_voxel_centers, voxel_centers.size() / 3 , d_occupied);
+
+                // GPUメモリからホストメモリへ結果をコピー
+                std::vector<int> occupied(voxel_centers.size());
+                cudaMemcpy(occupied.data(), d_occupied, voxel_centers.size() * sizeof(int), cudaMemcpyDeviceToHost);
+                
                 // 空隙率を計算...
                 int sum = 0;
                 for (int o : occupied) {
                     sum += o;
                 }
-                float porosity = 1.0 - float(sum) / num_voxels;
-                std::cout << entry.path() << ": " << porosity << std::endl;
+                float porosity = 1.0 - float(sum) / voxel_centers.size() ;
+                std::cout << path.filename() << ", " << counter_region << " : " << porosity << std::endl;
+
+                // GPUメモリを解放
+                cudaFree(d_particles);
+                cudaFree(d_voxel_centers);
+                cudaFree(d_occupied);
+
+                counter_region++;
+
             }
         }
     }
     return 0;
+
 }
